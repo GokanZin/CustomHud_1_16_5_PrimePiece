fb43534a0d87d32603b74f2146940dce182b8725
Gokan <138010470+GokanZin@users.noreply.github.com>
2024-09-23 18:06:31 -0300

    update[+]

--- src/main/java/br/com/gokan/customhud/events/HudEvents.java
+++ src/main/java/br/com/gokan/customhud/events/HudEvents.java
@@ -1,10 +1,11 @@
 package br.com.gokan.customhud.events;
 
 import br.com.gokan.customhud.Main;
+import br.com.gokan.customhud.handler.PlayerCapabilityHandler;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
 //import handler.PlayerCapabilityHandler;
-import br.com.gokan.customhud.handler.PlayerCapabilityHandler;
+//import br.com.gokan.customhud.handler.PlayerCapabilityHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.gui.FontRenderer;
@@ -55,7 +56,7 @@ public class HudEvents {
 
 
     private static void redeWorldText(Minecraft minecraft, FontRenderer render, PlayerEntity player, RenderGameOverlayEvent event, MatrixStack matrixStack) {
-        minecraft.font.draw(matrixStack, "§lREDE WORLD", 70, 18, 0xFFFFFF);
+        minecraft.font.draw(matrixStack, "§lREDE WORLD", 70 - 10, 18 - 10, 0xFFFFFF);
     }
 
     private static void staminaBar(Minecraft minecraft, FontRenderer render, PlayerEntity player, RenderGameOverlayEvent event, MatrixStack matrixStack) {
@@ -64,28 +65,33 @@ public class HudEvents {
         int stamina = PlayerCapabilityHandler.getStamina(player).intValue();
         int result = calcularTamanhoBarra((int) stamina, staminaMax, 149);
         RenderSystem.enableBlend();
-        AbstractGui.blit(matrixStack, 10, 31+10, 0, 55, result, 6, 256, 256); // Coordenadas x, y e tamanhos da textura
+        AbstractGui.blit(matrixStack, 0, 31+10 - 10, 0, 55, result, 6, 256, 256); // Coordenadas x, y e tamanhos da textura
         RenderSystem.disableBlend();
+
+        matrixStack.pushPose();
+        matrixStack.scale(0.69F, 0.69F, 1.0F); // Diminuir para 50% do tamanho original
+        minecraft.font.draw(matrixStack, "Stamina: " + stamina, 59 - 23, 18 + 27, 0xFFFFFF);
+        matrixStack.popPose();
     }
 
     private static void vidaBar(Minecraft minecraft, FontRenderer render, PlayerEntity player, RenderGameOverlayEvent event, MatrixStack matrixStack) {
         minecraft.getTextureManager().bind(texture);
-        int maxVida = PlayerCapabilityHandler.getMaxHealth(player).intValue();
-        float vida = player.getHealth();
+        int maxVida = (int) player.getMaxHealth();
+        int vida = (int) player.getHealth();
         int result = calcularTamanhoBarra((int) vida, maxVida, 147);
         RenderSystem.enableBlend();
-        AbstractGui.blit(matrixStack, 10, 31, 0, 45, result, 10, 256, 256); // Coordenadas x, y e tamanhos da textura
+        AbstractGui.blit(matrixStack, 0, 31 - 10, 0, 45, result, 10, 256, 256); // Coordenadas x, y e tamanhos da textura
         RenderSystem.disableBlend();
         matrixStack.pushPose();
         matrixStack.scale(0.7F, 0.7F, 1.0F); // Diminuir para 50% do tamanho original
-        minecraft.font.draw(matrixStack, "Vida:  " + vida, 59, 18 + 27, 0xFFFFFF);
+        minecraft.font.draw(matrixStack, "Vida:  " + vida, 59 - 10, 18 + 27 - 15, 0xFFFFFF);
         matrixStack.popPose();
     }
 
     private static void hudBar(Minecraft minecraft, FontRenderer render, PlayerEntity player, RenderGameOverlayEvent event, MatrixStack matrixStack) {
         minecraft.getTextureManager().bind(texture);
         RenderSystem.enableBlend();
-        AbstractGui.blit(matrixStack, 10, 10, 0, 0, 170, 45, 256, 256); // Coordenadas x, y e tamanhos da textura
+        AbstractGui.blit(matrixStack, 0, 0, 0, 0, 170, 45, 256, 256); // Coordenadas x, y e tamanhos da textura
         RenderSystem.disableBlend();
     }
 
--- src/main/java/br/com/gokan/customhud/handler/PlayerCapabilityHandler.java
+++ src/main/java/br/com/gokan/customhud/handler/PlayerCapabilityHandler.java
@@ -1,16 +1,19 @@
 package br.com.gokan.customhud.handler;
 
 import net.mcreator.trueprimepiece.TrueprimepieceModVariables;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
-import org.lwjgl.system.CallbackI;
 
 public class PlayerCapabilityHandler {
 
     public static Double getMaxHealth(PlayerEntity player) {
         Capability<TrueprimepieceModVariables.PlayerVariables> playerVariablesCap = TrueprimepieceModVariables.PLAYER_VARIABLES_CAPABILITY;
         LazyOptional<TrueprimepieceModVariables.PlayerVariables> playerVariables = player.getCapability(playerVariablesCap);
+        Double vidaMax = playerVariables.map(vars -> vars.maxhp).orElse(10.0);
         return playerVariables.map(vars -> vars.maxhp).orElse(10.0);
     }
 
